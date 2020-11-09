ef64366f1dbb5a59511de2c0929ff64badd42aed
Joseph Kawamura <54853856+jkawamura@users.noreply.github.com>
2020-11-09 02:26:41 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -6,47 +6,55 @@
 //gets the score and name of the latest player
 $score = $_POST['score'];
 $name = $_POST['name'];
-$servername = "localhost";
-$username = "gtstudent";
-$password = "";
-$dbname = "test";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+//opens the two files holding the names and scores and appends the player's scores 
+$scoreFile = fopen("scores.txt", "a+");
+$nameFile = fopen("names.txt", "a+");
+fwrite($scoreFile, $score . "\n" );
+fwrite($nameFile, $name . "\n");
+
+//turns the contents of each file into an array and typecasts the score array to integers
+$names = explode("\n", file_get_contents('names.txt'));
+$scores = explode("\n", file_get_contents('scores.txt'));
+$scores = array_map('intval', $scores);
+
+//closes the file
+fclose($scoreFile);
+
+//sorts the scores from largest to smallest with insertion sort. 
+//the names are sorted using the same swaps so that they are indexed to match their corresponding score
+for($i = 1; $i < count($scores); $i++){
+    $skey = $scores[$i];
+    $nkey = $names[$i];
+    $j = $i - 1;
+    while($j >= 0 && $skey > $scores[$j]){
+        $scores[$j+1] = $scores[$j];
+        $names[$j+1] = $names[$j];
+        $j -= 1;
+    } 
+    $scores[$j + 1] = $skey;
+    $names[$j + 1] = $nkey;
 }
 
-$query = "INSERT INTO jk_scores(username, score) VALUES('$name', '$score')";
-if ($conn->query($query) === TRUE) {
-  } else {
-    echo "Error inserting: " . $conn->error;
-  }
+//removes the last score from each array (\n automatically adds a blank line to the end of the file)
+$removed = array_pop($names);
+$removed2 = array_pop($scores);
 
 
-$query = "SELECT ROW_NUMBER() OVER(ORDER BY score desc) AS row, username, score FROM jk_scores;";
-$result = $conn->query($query);
-
-echo '<div><table><tr><th>Rank</th><th>Score</th><th>Name</th></tr><caption>High Scores</caption>';
-
-$i = 0;
-while($row = $result->fetch_assoc()){
-    if($i == 10){
-        break;
+//finds the user's score so it can be ranked separately
+$index = 0;
+for($i = 0; $i < count($scores); $i++){
+    if($scores[$i] == intval($score) && $names[$i] == $name){
+        $index = $i + 1;
     }
-    $i += 1;
-    echo '<tr><td>' . $row["row"] . '</td><td>' . $row["score"] . '</td><td>' . $row["username"] . '</td></tr>';
 }
 
-$i = 0;
-while($row = $result->fetch_assoc()) {
-    $i += 1;
-    if($row['username'] == $name && $row['score'] == $score) {
-        echo "<tr><td class='user'>" . $row['row'] . "</td><td class='user'>" . $row['score'] . "</td><td class='user'>" . $row['username'] . "</td></tr>";
-        break;
-    }
+//prints out the top 10 scores in a table with the users score at the bottom. Has a button returning to welcome page.
+echo '<div><table><tr><th>Rank</th><th>Name</th><th>Score</th></tr><caption>High Scores</caption>';
+for($i=0; $i<min(10, count($names));$i++){
+    echo '<tr><td>' . ($i + 1) . '</td><td>' . $names[$i] . '</td><td>' . $scores[$i] . '</td></tr>';
 }
+echo "<tr><td class='user'>" . $index . "</td><td class='user'>" . $name . "</td><td class='user'>" . $score . "</td></tr>";
 echo "</table><br><br>";
 echo "<a href='index.html' class='button'>Play Again</a></div>";
 ?>
