ef3da0f85683e7c212ac4f120c6cb316bf471397
Joseph Kawamura <54853856+jkawamura@users.noreply.github.com>
2020-11-09 01:34:19 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<body>
+<link rel="stylesheet" href="php.css">
+<?php
+//gets the score and name of the latest player
+$score = $_POST['score'];
+$name = $_POST['name'];
+$servername = "localhost";
+$username = "gtstudent";
+$password = "";
+$dbname = "test";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$query = "INSERT INTO jk_scores(username, score) VALUES('$name', '$score')";
+if ($conn->query($query) === TRUE) {
+  } else {
+    echo "Error inserting: " . $conn->error;
+  }
+
+
+$query = "SELECT ROW_NUMBER() OVER(ORDER BY score desc) AS row, username, score FROM jk_scores;";
+$result = $conn->query($query);
+
+echo '<div><table><tr><th>Rank</th><th>Score</th><th>Name</th></tr><caption>High Scores</caption>';
+
+$i = 0;
+while($row = $result->fetch_assoc()){
+    if($i == 10){
+        break;
+    }
+    $i += 1;
+    echo '<tr><td>' . $row["row"] . '</td><td>' . $row["score"] . '</td><td>' . $row["username"] . '</td></tr>';
+}
+
+$i = 0;
+while($row = $result->fetch_assoc()) {
+    $i += 1;
+    if($row['username'] == $name && $row['score'] == $score) {
+        echo "<tr><td class='user'>" . $row['row'] . "</td><td class='user'>" . $row['score'] . "</td><td class='user'>" . $row['username'] . "</td></tr>";
+        break;
+    }
+}
+echo "</table><br><br>";
+echo "<a href='index.html' class='button'>Play Again</a></div>";
+?>
+</body>
+</html>
+
