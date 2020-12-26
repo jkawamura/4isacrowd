ab3905caf65b46f47b87627c9bbb67a7a841d332
Joseph Kawamura <joseph.kawamura@gmail.com>
2020-12-25 20:14:12 -0700

    switched databases

--- scoring.php
+++ scoring.php
@@ -7,25 +7,27 @@
 $score = $_POST['score'];
 $name = $_POST['name'];
 $servername = "localhost";
-$username = "gtstudent";
-$password = "";
-$dbname = "test";
+$username = "jkawamur_1";
+$password = "zJJxva8f'+hN";
+$dbname = "jkawamur_4isacrowd";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
+  exit;
 }
 
-$query = "INSERT INTO jk_scores(username, score) VALUES('$name', '$score')";
+$query = "INSERT INTO highscores(username, score) VALUES('$name', '$score')";
 if ($conn->query($query) === TRUE) {
   } else {
     echo "Error inserting: " . $conn->error;
   }
-
-
-$query = "SELECT ROW_NUMBER() OVER(ORDER BY score desc) AS row, username, score FROM jk_scores;";
+  
+$query = "set @row_number = 0;"; 
+$conn->query($query); 
+$query = "SELECT (@row_number:=@row_number + 1) as row, username, score from highscores order by score desc;";
 $result = $conn->query($query);
 
 echo '<div><table><tr><th>Rank</th><th>Score</th><th>Name</th></tr><caption>High Scores</caption>';
